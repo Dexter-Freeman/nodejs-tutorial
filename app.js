3d594a2836577e99aaa546e5a45135af4ff901c1
Dexter Freeman <vse.pisma.tut@gmail.com>
2020-04-19 19:42:11 +0300

    Fs module

--- app.js
+++ app.js
@@ -84,7 +84,77 @@ myEmitter.on('event', function firstListener() {
   persons.forEach(p => p.on('speak', (msg) => console.log(p.name + ' said ' + msg)));
   person1.emit('speak', 'hey you!');
 
-  // Однако в синтаксисе ES6 наследование классов делается намного проще
-  // class Person extends events.EventEmitter
+// Однако в синтаксисе ES6 наследование классов делается намного проще
+// class Person extends events.EventEmitter
 
-  // __________________________________________
+// __________________________________________
+
+// File System
+
+// встроенный модуль fs позволяет работать с файловой системой: читать/записывать файлы/папки
+const fs = require('fs');
+
+// прочитаем файл fsRead.txt находящийся в той же директории 
+let readMe = fs.readFileSync('fsRead.txt', 'utf8');
+console.log(readMe);
+// метод readFileSync является "блокирующим", т.е. он не асинхронный, и исполнение скрипта
+// не продолжится до завершения работы readFileSync. У него есть и асинхронный аналог
+// readFileSync принимает ("путь к файлу", "кодировку файла"), без кодировки он прочитает
+// нули и единицы
+
+// создадим новый файл
+fs.writeFileSync('fsWrite.txt', readMe);
+// файл fsWrite.txt был создан
+// writeFileSync также является блокирующим методом
+
+// воспользуемся асинхронными методами для чтения/записи файлов
+// асинхронные методы отличаются от синхронных тем, что им необходимо передвать коллбек,
+// который будет вызван по завершению работы метода, этот коллбек принимает два параметра - 
+// ошибку и данные
+fs.readFile('fsRead.txt', 'utf8', (err, data) => {
+  if (err) return console.error(err);
+  console.log(data);
+});
+
+console.log('readFile don\'t blocking code'); // отработает раньше чем прочитается файл
+
+fs.writeFile('fsWriteAsync.txt', readMe, err => {
+  return err ? console.error(err) : null
+});
+
+// для удаления файлов - метод unlink
+fs.unlink('fsWrite.txt', (err) => err ? console.log(err) : null);
+
+// Для создания директорию синхронно, используем метод mkdirSync('Название директории')
+fs.mkdirSync('stuff');
+// директория была создана
+// чтобы удалить директорию - rmdirSync, это тоже синхронный метод
+fs.rmdirSync('stuff');
+// директория была удалена
+
+// для того чтобы не блокировать код, нужно использовать асинхронные методы
+// не забываем, о том что при использовании асинхронных методов, необходимо в них 
+// передавать коллбек
+
+fs.mkdir('stuff', err => {
+  if (err) return console.log(err);
+  console.log('stuff directory has been created');
+
+  // создадим в этой директории файл
+  let readMe = fs.readFileSync('fsRead.txt', 'utf8');
+  fs.writeFile('fsWriteAsync.txt', readMe, err => {
+    return err ? console.log(err) : console.log('file was writed');
+
+  });
+});
+
+// теперь удалим этой файл и директорию
+fs.unlink('./stuff/fsWriteAsync.txt', err => {
+  // после удаления файла, директория станет пустой
+  // При попытке удалить не пустую директорию, будет выброшена ошибка
+  console.log('file removed');
+  fs.rmdir('stuff', err => err ? console.log(err) : console.log('folder removed'));
+  // удалили директорию
+});
+
+// __________________________________________
